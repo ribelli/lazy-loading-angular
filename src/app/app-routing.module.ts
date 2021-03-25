825d361e288fa4acd2cc0cf7af6564fcbaf14de5
ribelli <ribelli.oi@gmail.com>
2021-03-25 14:02:42 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '@pages/home/home.component';
+import { MoviesComponent } from '@pages/movies/movies.component';
+import { SeriesComponent } from '@pages/series/series.component';
+import { MusicComponent } from '@pages/music/music.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent,
+  },
+  {
+    path: 'series',
+    component: SeriesComponent,
+  },
+  {
+    path: 'music',
+    component: MusicComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from '@app/app.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { MoviesComponent } from '@pages/movies/movies.component';
+import { SeriesComponent } from '@pages/series/series.component';
+import { MusicComponent } from '@pages/music/music.component';
+
+export const COMPONENTS = [
+  AppComponent,
+  HomeComponent,
+  MoviesComponent,
+  SeriesComponent,
+  MusicComponent
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    COMPONENTS,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
